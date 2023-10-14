7d0eb77907ef49423b9ca61e95776c5e9bfd9c28
Brandon <48961783+AggressiveCombustion@users.noreply.github.com>
2023-10-13 22:56:15 -0500

    visual overhaul

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -19,6 +19,9 @@ public class Laser : InteractableObject
     // Update is called once per frame
     void Update()
     {
+        if (line == null)
+            return;
+
         line.SetPosition(0, line.transform.position);
 
         RaycastHit2D hit = Physics2D.Raycast(line.transform.position, line.transform.up * -1, 100, mask);
