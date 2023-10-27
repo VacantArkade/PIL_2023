d566c6662e9b74ec662e4a29957399df150433e9
Brandon <48961783+AggressiveCombustion@users.noreply.github.com>
2023-10-27 12:28:01 -0500

    menus and saving data

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class GameManager : MonoBehaviour
 
     public float volumeSFX = 1.0f;
     public float volumeBGM = 1.0f;
+
+    public AudioMixer mixer;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,8 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+
+        LoadCurrentSettings();
     }
 
     // Update is called once per frame
@@ -101,5 +106,36 @@ public class GameManager : MonoBehaviour
         int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
         int nextLevelIndex = currentLevelIndex + 1;
         SceneManager.LoadScene(nextLevelIndex);
+    }
+
+    public void LoadCurrentSettings()
+    {
+        Debug.Log("Loading saved settings");
+        var bgm = PlayerPrefs.GetFloat("bgm", 0.8f);
+        var sfx = PlayerPrefs.GetFloat("sfx", 0.8f);
+        string saved_resolution = PlayerPrefs.GetString("resolution", "640 X 480");
+        //resolution.value = resolution.options.FindIndex(option => option.text == saved_resolution);
+        bool fullscreen = PlayerPrefs.GetInt("fullscreen", 0) == 1? true : false;
+
+        SetResolution(saved_resolution, fullscreen);
+        SetAudio(sfx, bgm);
+    }
+
+    public void SetResolution(string res, bool isFullscreen)
+    {
+        int x_index = res.IndexOf("X");
+        string width = res.Substring(0, x_index - 1).Trim();
+        string height = res.Substring(x_index + 1).Trim();
+
+        Screen.SetResolution(int.Parse(width), int.Parse(height), isFullscreen);
+
+        FindObjectOfType<CameraFixer>()?.FixScreen();
+    }
+
+    public void SetAudio(float sfx, float bgm)
+    {
+        Debug.Log("SETTING AUDIO");
+        mixer.SetFloat("SFX", /*Mathf.Log10(sfx) * 20*/ (- 60 + sfx * 80)); // -80 + (sfx * 80)
+        mixer.SetFloat("BGM", (-60 + bgm * 80));
     }
 }
--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
     [SerializeField]
+    AudioMixer mixer;
+    [SerializeField]
     TMP_Dropdown resolution;
     [SerializeField]
     TMP_Dropdown windowType;
+    [SerializeField]
+    Slider sliderSFX;
+    [SerializeField]
+    Slider sliderBGM;
+
+    bool dontSave = true;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +41,7 @@ public class SettingsMenu : MonoBehaviour
     private void OnEnable()
     {
         Time.timeScale = 0;
+        LoadCurrentSettings();
     }
 
     private void OnDestroy()
@@ -39,21 +49,65 @@ public class SettingsMenu : MonoBehaviour
         Time.timeScale = 1;
     }
 
+    public void LoadCurrentSettings()
+    {
+        dontSave = true;
+        Debug.Log("applying settings to menu");
+        sliderBGM.value = PlayerPrefs.GetFloat("bgm", 0.8f);
+        sliderSFX.value = PlayerPrefs.GetFloat("sfx", 0.8f);
+        string saved_resolution = PlayerPrefs.GetString("resolution", "640 X 480");
+        resolution.value = resolution.options.FindIndex(option => option.text == saved_resolution);
+        windowType.value = PlayerPrefs.GetInt("fullscreen", 0);
+        dontSave = false;
+
+        //ApplyResolution();
+        //ApplyAudioChanges();
+    }
+
     public void ApplyResolution()
     {
         var res_choice = resolution.options[resolution.value].text;
         //Debug.Log(res_choice);
 
-        int x_index = res_choice.IndexOf("X");
+        /*
+         * int x_index = res_choice.IndexOf("X");
         string width = res_choice.Substring(0, x_index - 1).Trim();
         string height = res_choice.Substring(x_index + 1).Trim();
+        */
 
         //Debug.Log("[" + width + "], [" + height + "]");
 
         bool isFullscreen = windowType.value == 1;
 
-        Screen.SetResolution(int.Parse(width), int.Parse(height), isFullscreen);
+        /*Screen.SetResolution(int.Parse(width), int.Parse(height), isFullscreen);
 
-        FindObjectOfType<CameraFixer>()?.FixScreen();
+        FindObjectOfType<CameraFixer>()?.FixScreen();*/
+
+        GameManager.instance.SetResolution(res_choice, isFullscreen);
+
+        SavePreferences();
+    }
+
+    public void ApplyAudioChanges()
+    {
+        /*mixer.SetFloat("SFX", Mathf.Log10(sliderSFX.value) * 20);
+        mixer.SetFloat("BGM", Mathf.Log10(sliderBGM.value) * 20);*/
+
+        GameManager.instance.SetAudio(sliderSFX.value, sliderBGM.value);
+
+        SavePreferences();
+    }
+
+    public void SavePreferences()
+    {
+        if (dontSave)
+            return;
+
+        PlayerPrefs.SetString("resolution", Screen.currentResolution.width + " X " + Screen.currentResolution.height);
+        PlayerPrefs.SetInt("fullscreen", Screen.fullScreen ? 1 : 0);
+        PlayerPrefs.SetFloat("sfx", sliderSFX.value);
+        PlayerPrefs.SetFloat("bgm", sliderBGM.value);
+
+        Debug.Log("saved preferences!");
     }
 }
